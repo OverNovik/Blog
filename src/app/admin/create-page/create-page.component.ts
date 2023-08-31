7f2e4cd6dce1ea0ddfbf4ff8178e5ec1e044a5aa
Aliaxander Novikov <nerevarin11@gmail.com>
2023-08-31 15:05:11 +0300

    feat: create alert plugin

--- src/app/admin/create-page/create-page.component.ts
+++ src/app/admin/create-page/create-page.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Post } from 'src/app/shared/interfaces';
 import { PostService } from 'src/app/shared/post.service';
+import { AlertService } from '../shared/services/alert.service';
 
 @Component({
   selector: 'app-create-page',
@@ -11,7 +12,7 @@ import { PostService } from 'src/app/shared/post.service';
 export class CreatePageComponent implements OnInit {
   form!: FormGroup;
 
-  constructor(private postService: PostService) {}
+  constructor(private postService: PostService, private alert: AlertService) {}
 
   ngOnInit(): void {
       this.form = new FormGroup({
@@ -35,6 +36,7 @@ export class CreatePageComponent implements OnInit {
 
     this.postService.create(post).subscribe(() => {
       this.form.reset();
+      this.alert.success('Post was created!')
     })
   }
 }
--- src/app/admin/edit-page/edit-page.component.ts
+++ src/app/admin/edit-page/edit-page.component.ts
@@ -4,6 +4,7 @@ import { ActivatedRoute, Params } from '@angular/router';
 import { Subscription, switchMap } from 'rxjs';
 import { Post } from 'src/app/shared/interfaces';
 import { PostService } from 'src/app/shared/post.service';
+import { AlertService } from '../shared/services/alert.service';
 
 @Component({
   selector: 'app-edit-page',
@@ -18,7 +19,8 @@ export class EditPageComponent implements OnInit, OnDestroy {
 
   constructor(
     private route: ActivatedRoute,
-    private postsService: PostService
+    private postsService: PostService,
+    private alert: AlertService
   ) {}
 
   ngOnInit(): void {
@@ -55,6 +57,7 @@ export class EditPageComponent implements OnInit, OnDestroy {
       title: this.form.value.title
     }).subscribe(() => {
       this.submitted = false
+      this.alert.success('Post was updated!')
     })
   }
 }
